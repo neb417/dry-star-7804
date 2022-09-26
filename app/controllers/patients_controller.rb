b4a85af59fad17d1e077ef83ae1b2756b7c995c3
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-09-26 10:54:52 -0600

    add tests to patient index page.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,6 +3,7 @@ class PatientsController < ApplicationController
   def destroy
     @doctor = Doctor.find(params[:doctor_id])
     @doctor.patients.destroy(params[:id])
+    
     redirect_to doctor_path(@doctor.id)
   end
 end
